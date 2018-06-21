cb3b67451af0617e8c1acf4a5a8a1aa04e754bd6
杜炎峰 <20171105145du@admindeimac-62.local>
2018-06-21 09:17:21 +0800

    Initial Commit

---
+++ hello word! 2018.06.21/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello word! 2018.06.21
+//
+//  Created by 杜炎峰 on 2018/6/21.
+//  Copyright © 2018年 杜炎峰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
